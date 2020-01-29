77ace28c2aef652ddf34c050470cbf0f308a0777
Reinaldo S.G <reinaldogunawan@yahoo.com>
2020-01-29 19:45:25 +0700

    add some model

---
+++ application/models/TransaksiModel.php
@@ -0,0 +1,34 @@
+<?php
+class TransaksiModel extends CI_Model {
+
+    public $idCustomer;
+    public $nama;
+    public $nomor;
+    public $email;
+
+	public function __construct(){
+		$this->load->database();
+    }
+    
+    public function get_all($id = NULL){
+        $this->db->select();
+		$this->db->from('customer');
+		if ($id != NULL){
+			$this->db->where('IDCustomer',$id);
+		}
+		$query = $this->db->get();
+		return $query->result_array();
+    }
+
+    public function insert_one($data){
+        $this->db->insert('user', $data);
+        if ($this->db->affected_rows() > 0 ) {
+			$return_message = 'success';
+		}else{
+			$return_message = 'failed';
+		}
+		return $return_message;
+    }
+
+}
+?>
